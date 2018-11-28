75a5169b5fcbed048dde0db2b3286fb07eded689
Lucas Blin <blin.lucas2@gmail.com>
2018-11-28 21:52:47 +0100

    parsing data to csv

--- src/main/java/com/javahelps/pcapparser/Main.java
+++ src/main/java/com/javahelps/pcapparser/Main.java
@@ -5,36 +5,55 @@ import io.pkts.Pcap;
 import io.pkts.buffer.Buffer;
 import io.pkts.packet.Packet;
 import io.pkts.packet.TCPPacket;
-import io.pkts.packet.UDPPacket;
 import io.pkts.protocol.Protocol;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        final Pcap pcap = Pcap.openStream("PATH TO FILE");
+        final Pcap pcap = Pcap.openStream("parse.pcap");
+     
 
         pcap.loop(new PacketHandler() {
-            public boolean nextPacket(Packet packet) throws IOException {
+            
+            StringBuffer csvData = new StringBuffer("");
 
+            public boolean nextPacket(Packet packet) throws IOException {
                 if (packet.hasProtocol(Protocol.TCP)) {
+
+                    final PrintWriter pw = new PrintWriter(new File("result.csv"));
+ 
+                    StringBuffer csvHeader = new StringBuffer("");
+                    csvHeader.append("Login:password\n");
+            
+                    pw.write(csvHeader.toString());
 
                     TCPPacket tcpPacket = (TCPPacket) packet.getPacket(Protocol.TCP);
                     Buffer buffer = tcpPacket.getPayload();
                     if (buffer != null) {
                         final String[] arr = buffer.toString().split(" ", 2);                       
                         if(arr[0].equals("USER")){
-                            System.out.println("Utilisateur : " + arr[1]);
+                            csvData.append(arr[1].replace("\r\n",""));
                         }
                         if(arr[0].equals("PASS")){
-                            System.out.println("Mot de passe : " + arr[1]);
+                            csvData.append(":"+arr[1]);
                         }
+        
                     }
+
+                    pw.write(csvData.toString());
+                    pw.close();
+
+
                 }
+                
                 return true;
+                
             }
+
         });
     }
 }
